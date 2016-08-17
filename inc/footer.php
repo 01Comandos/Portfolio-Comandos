6c080b02734de6001c044c9cb3428dbc5623cea2
Comandos <00comandos@gmail.com>
2016-08-16 21:48:59 -0400

    Archivos preparados para rediseño portfolios

---
+++ inc/footer.php
@@ -0,0 +1,84 @@
+<footer class="footerContainer">
+	<!--LOGOTIPO-->
+	<section class="footerContainer-item">
+		<figure>
+			<img src="img/camersur-logo.png" alt="camersur logotipo">
+		</figure>
+	</section>
+
+	<!--EMAIL - DIRECCION-->
+	<section class="footerContainer-item">
+		<!--EMAIL-->
+		<div class="options">
+			<figure>
+				<span class="icon-CamersurNoticias_email"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></span>
+			</figure>
+			<h5>hola@camersur.com</h5>
+		</div>
+		
+		<!--DIRECCION-->
+		<div class="options">
+			<figure>
+				<span class="icon-CamersurNoticias_direccion"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span><span class="path20"></span><span class="path21"></span><span class="path22"></span><span class="path23"></span><span class="path24"></span><span class="path25"></span><span class="path26"></span><span class="path27"></span><span class="path28"></span><span class="path29"></span><span class="path30"></span><span class="path31"></span><span class="path32"></span><span class="path33"></span><span class="path34"></span><span class="path35"></span></span>
+			</figure>
+			<h5>Calle Rio Caribe de Cumaná 22 <br>6101 VENEZUELA</h5>
+		</div>
+	</section>
+
+	<!--UBICACION Y REDES SOCIALES-->
+	<section class="footerContainer-item">
+		<!--UBICACION-->
+		<div class="options">
+			<a href="#!" target="_blank">
+				<p>Ubicanos en 
+					<br><strong>GOOGLE MAPS</strong>
+				</p>
+				<figure>
+					<span class="icon-CamersurNoticias_ubication"></span>
+				</figure>
+			</a>
+		</div>
+
+		<!--SOCIAL MEDIA-->
+		<div class="optionsSocial">
+			<p>Siguenos en</p>
+			<ul>
+				<!--LINKEDIN-->
+				<li>
+					<a href="http://es.slideshare.net/camersur" target="_blank">
+						<span class="icon-CamersurNoticias_linkedin2"></span>
+					</a>
+				</li>
+				<!--FACEBOOK-->
+				<li>
+					<a href="https://www.facebook.com/ca.mer.sur.com.ve/?fref=ts" target="_blank">
+						<span class="icon-CamersurNoticias_facebook2"></span>
+					</a>
+				</li>
+				<!--TWITTER-->
+				<li>
+					<a href="https://twitter.com/camersur" target="_blank">
+						<span class="icon-CamersurNoticias_twitter2"></span>
+					</a>
+				</li>
+				<!--YOUTUBE-->
+				<li>
+					<a href="https://www.youtube.com/channel/UCIEFGplXnr7y3wsKzY83iRg" target="_blank">
+						<span class="icon-CamersurNoticias_youtube2"></span>
+					</a>
+				</li>
+				<!--INSTAGRAM-->
+				<li>
+					<a href="" target="_blank">
+						<span class="icon-CamersurNoticias_instagram2"></span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<!--DERECHOS RESERVADOS-->
+	<section class="footerContainer-item">
+		<h6>© CAMERSUR.com Una web de <a href="#!" target="_blank">CESTICOM</a></h6>
+	</section>
+</footer>
---
+++ inc/head_common.php
@@ -0,0 +1,31 @@
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="img/favicon.ico" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Camersur Noticias</title>
+    <meta name="author" content="@01Comandos">
+    <meta name="description" content="Camersur noticias, un portal de economía, politica, negocios y empresas enfocado a Latinoamérica. Pensando en ti.">
+    <!-- FIN DE METADATOS -->
+
+    <!-- ESTILOS -->    
+    <link href="css/materialize.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- FIN DE ESTILOS -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    <!--GOOGLE ANALYTICS-->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-80349959-1', 'auto');
+        ga('send', 'pageview');
+    </script>
