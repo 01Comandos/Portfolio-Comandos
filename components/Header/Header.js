fbf2e824e5c0f2c93b96a1b1fd9fabf6d33fbce4
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-05-21 17:50:16 -0500

    Add Hero mobile

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,22 +1,14 @@
-import Image from 'next/image'
-import styles from "./Header.module.css"
+import Image from "next/image";
+import styles from "./Header.module.css";
 
 const Header = () => (
-    <div className={styles.container}>
-      <Image
-        src="/logo/logo-isotipo-white.svg"
-        width={51}
-        height={51}
-      />
-      <div className={styles.rightOptions}>
-        <button class="black">Contact</button>
-        <Image
-            src="/icons/icon-menu.svg"
-            width={28}
-            height={19}
-        />
-      </div>
+  <div className={styles.container}>
+    <Image src="/logo/logo-isotipo-white.svg" width={51} height={51} />
+    <div className={styles.rightOptions}>
+      <button class="black">Contact</button>
+      <Image src="/icons/icon-menu.svg" width={28} height={19} />
     </div>
+  </div>
 );
 
 export default Header;
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,8 +1,13 @@
-import styles from "./Hero.module.css"
+import Image from "next/image";
+import styles from "./Hero.module.css";
 
 const Hero = () => (
-  <div>
-    Hero
+  <div className={styles.container}>
+    <Image className={styles.picture} layout="fill" objectFit="contain" src="/images/coming-soon-mobile.png" />
+    <div className={styles.textContainer}>
+      <h1 className={styles.title}>Innovating experiences for humans</h1>
+      <h3 className={styles.subtitle}>Let’s create something together ></h3>
+    </div>
   </div>
 );
 
