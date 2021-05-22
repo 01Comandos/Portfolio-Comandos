0fdc374d3a91a74064a3e23de733ee8706f70456
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-05-22 16:03:29 -0500

    Added footer design

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,9 +1,15 @@
-import styles from "./Footer.module.css"
+import Image from "next/image";
+import SocialNetworks from "../SocialNetworks/SocialNetworks";
+import styles from "./Footer.module.css";
 
-const Footer = ({ children }) => (
-    <footer>
-        Footer
-    </footer>
+const Footer = () => (
+  <footer className={styles.container}>
+    <SocialNetworks />
+    <div className={styles.brand}>
+      <Image src="/logo/logo-isotipo-white.svg" width={51} height={51} />
+      <span>2021 © Comandos.me</span>
+    </div>
+  </footer>
 );
 
 export default Footer;
