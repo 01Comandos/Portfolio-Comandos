30b4366231a473f6d1f95217e42164189a88be9f
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-05-23 01:48:13 -0500

    Added contact section

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,7 +1,11 @@
 import styles from "./Contact.module.css"
 
 const Contact = () => {
-  return <div>Contact section</div>
+  return <div className={styles.container}>
+    <h2 className={styles.title}>Let’s <span>talk</span></h2>
+    <p className={styles.text}>Let us know who you are and what you’re up to.</p>
+    <button className={`black ${styles.contactButton}`}>Contact</button>
+  </div>
 };
 
 export default Contact;
