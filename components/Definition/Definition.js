b38f3cba393a19f55b61dc2cc7927c1a55b52d7f
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-05-23 01:38:41 -0500

    Added definition section

--- components/Definition/Definition.js
+++ components/Definition/Definition.js
@@ -1,7 +1,21 @@
-import styles from "./Definition.module.css"
+import styles from "./Definition.module.css";
 
 const Definition = () => {
-  return <div>Definition section</div>
+  return (
+    <div className={styles.container}>
+      <img className={styles.logo} src="/logo/logo-white.svg" width={192} height={42} />
+      <p className={styles.description}>
+        Comandos has more than 10 years of experience creating innovate products
+        through human-centered desing. The most effective team of designers and
+        developers to achieve the maximum potential of your idea.
+      </p>
+      <div className={styles.knowMore}>
+        <span>I want to know more</span>
+        <img src="/icons/icon-arrow.svg" />
+      </div>
+      <div className={styles.horizontalLine}></div>
+    </div>
+  );
 };
 
 export default Definition;
