45eb1123fb5f576d201b6f9943d022c3b68a262d
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-05-23 02:59:43 -0500

    Added responsive design for header component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,42 @@
-import Image from "next/image";
+import Link from "next/link";
 import styles from "./Header.module.css";
+
+const menuOptions = [
+  { name: "Home", link: "/" },
+  { name: "Projects", link: "/projects" },
+  { name: "Methodology", link: "/methodology" },
+  { name: "Podcast", link: "/podcast" }
+];
 
 const Header = () => (
   <div className={styles.container}>
-    <Image src="/logo/logo-isotipo-white.svg" width={51} height={51} />
+    <img
+      className={styles.logoMobile}
+      src="/logo/logo-isotipo-white.svg"
+      width={51}
+      height={51}
+    />
+    <img
+      className={styles.logo}
+      src="/logo/logo-white.svg"
+      width={183}
+      height={40}
+    />
+    <div className={styles.menu}>
+      {menuOptions.map(item => (
+        <Link href={item.link}>
+          <a>{item.name}</a>
+        </Link>
+      ))}
+    </div>
     <div className={styles.rightOptions}>
       <button className="black">Contact</button>
-      <Image src="/icons/icon-menu.svg" width={28} height={19} />
+      <img
+        className={styles.menuIcon}
+        src="/icons/icon-menu.svg"
+        width={28}
+        height={19}
+      />
     </div>
   </div>
 );
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -4,7 +4,7 @@ import styles from "./Hero.module.css";
 
 const Hero = () => (
   <div className={styles.container}>
-    <Image className={styles.picture} layout="fill" objectFit="contain" src="/images/coming-soon-mobile.png" />
+    <img className={styles.picture} src="/images/coming-soon-mobile.png" />
     <div className={styles.textContainer}>
       <h1 className={styles.title}>Innovating experiences for humans</h1>
       <h3 className={styles.subtitle}>Let’s create something together ></h3>
