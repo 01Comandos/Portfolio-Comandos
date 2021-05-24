44d334fafc78b8d47ac9d8e389a90eb0ad4c3403
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-05-24 01:07:14 -0500

    Add responsive design to definition section

--- components/Definition/Definition.js
+++ components/Definition/Definition.js
@@ -2,19 +2,21 @@ import styles from "./Definition.module.css";
 
 const Definition = () => {
   return (
-    <div className={styles.container}>
-      <img className={styles.logo} src="/logo/logo-white.svg" width={192} height={42} />
-      <p className={styles.description}>
-        Comandos has more than 10 years of experience creating innovate products
-        through human-centered desing. The most effective team of designers and
-        developers to achieve the maximum potential of your idea.
-      </p>
-      <div className={styles.knowMore}>
-        <span>I want to know more</span>
-        <img src="/icons/icon-arrow.svg" />
+    <section className={styles.container}>
+      <img className={styles.logo} src="/logo/logo-white.svg" alt="Logotipo Comandos" width={192} height={42} />
+      <div>
+        <p className={styles.description}>
+          Comandos has more than 10 years of experience creating innovate products
+          through human-centered desing. The most effective team of designers and
+          developers to achieve the maximum potential of your idea.
+        </p>
+        <div className={styles.knowMore}>
+          <span>I want to know more</span>
+          <img src="/icons/icon-arrow.svg" />
+        </div>
       </div>
       <div className={styles.horizontalLine}></div>
-    </div>
+    </section>
   );
 };
 
