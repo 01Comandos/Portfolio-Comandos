90507b78224a9613a663f86f146ffa13ba6953d9
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-05-24 01:15:10 -0500

    Added responsive design to contact section

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,11 +1,13 @@
 import styles from "./Contact.module.css"
 
 const Contact = () => {
-  return <div className={styles.container}>
-    <h2 className={styles.title}>Let’s <span>talk</span></h2>
-    <p className={styles.text}>Let us know who you are and what you’re up to.</p>
+  return <section className={styles.container}>
+    <div className={styles.description}>
+      <h2 className={styles.title}>Let’s <span>talk</span></h2>
+      <p className={styles.text}>Let us know who you are and what you’re up to.</p>
+    </div>
     <button className={`black ${styles.contactButton}`}>Contact</button>
-  </div>
+  </section>
 };
 
 export default Contact;
