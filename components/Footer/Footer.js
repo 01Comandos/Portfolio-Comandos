48d3b7706baf02d5ded5e23fffa5a7546703a247
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-05-24 01:24:02 -0500

    Added responsive design for footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 
 const Footer = () => (
   <footer className={styles.container}>
-    <SocialNetworks />
+    <SocialNetworks className={styles.socialLinks} />
     <div className={styles.brand}>
       <Image src="/logo/logo-isotipo-white.svg" width={45} height={45} />
       <span>2021 © Comandos.me</span>
