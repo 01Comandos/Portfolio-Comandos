103f07388953f9188df1c5b0227a789d94477670
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-06-04 17:15:09 -0500

    Fixes applied

--- components/Project/Project.js
+++ components/Project/Project.js
@@ -2,23 +2,22 @@ import { useState } from "react";
 import styles from "./Project.module.css";
 
 const Project = ({ isMobile, pictures, name, description }) => {
-  const [isIconVisible, setIconVisibility] = useState(false);
+  const [isHover, setHover] = useState(false);
 
   return (
-    <div className={styles.container}>
+    <article
+      className={styles.container}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <img src={pictures.mobile} className={styles.pictureMobile} />
-      <img
-        src={pictures.desktop}
-        className={styles.picture}
-        onMouseEnter={() => setIconVisibility(true)}
-        onMouseLeave={() => setIconVisibility(false)}
-      />
+      <img src={pictures.desktop} className={`${styles.picture} ${isHover ? styles.animation : null }`} />
       <h4 className={styles.title}>{name}</h4>
       <div className={styles.textContainer}>
         <p className={styles.description}>{description}</p>
-        {(isMobile || isIconVisible) && <img src="/icons/icon-arrow.svg" />}
+        {(isMobile || isHover) && <img src="/icons/icon-arrow.svg" />}
       </div>
-    </div>
+    </article>
   );
 };
 
