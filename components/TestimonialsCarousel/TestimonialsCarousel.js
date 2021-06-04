2ec87069c86be0837dceda24ec1549bc706a62ed
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-06-04 17:03:57 -0500

    Added new testimonials carousel

---
+++ components/TestimonialsCarousel/TestimonialsCarousel.js
@@ -0,0 +1,77 @@
+import { useState, useEffect, useRef } from "react";
+import styles from "./TestimonialsCarousel.module.css";
+
+const delay = 15000;
+
+const TestimonialCarousel = ({ testimonials, color = "tertiary" }) => {
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <section className={`${styles.container} ${styles[`${color}Container`]}`}>
+      <div
+        className={styles.slideshowSlider}
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {testimonials.map((testimonial, index) => (
+          <article key={index} className={styles.slide}>
+            {testimonial.title && <h3 className={styles.title}>{testimonial.title}</h3>}
+            <p className={styles.text}>{testimonial.feedback}</p>
+            <div className={styles.customers}>
+              <img
+                className={styles.customerPicture}
+                src={testimonial.customer.picture}
+                alt={testimonial.customer.name}
+              />
+              <div className={styles.customerInfo}>
+                <span className={styles.customerName}>
+                  {testimonial.customer.name}
+                </span>
+                <span className={styles.customerPosition}>
+                  {testimonial.customer.position}
+                </span>
+              </div>
+            </div>
+          </article>
+        ))}
+      </div>
+
+      <div className={styles.slideshowDots}>
+        {testimonials.map((_, idx) => (
+          <div
+            key={idx}
+            className={`${styles.slideshowDot}${
+              index === idx ? " " + styles.active : ""
+            }`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default TestimonialCarousel;
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,10 @@ import Hero from "../components/Hero/Hero";
 import Companies from "../components/Companies/Companies";
 import Projects from "../components/Projects/Projects";
 import Expertise from "../components/Expertise/Expertise";
-import Testimonials from "../components/Testimonials/Testimonials";
+import TestimonialsCarousel from "../components/TestimonialsCarousel/TestimonialsCarousel";
 import Definition from "../components/Definition/Definition";
 import Contact from "../components/Contact/Contact";
+import TestimonialsList from "../content/testimonials.json"
 import { isMobile } from "../utils";
 import styles from "../styles/Home.module.css";
 
@@ -28,7 +29,7 @@ const Home = ({ isMobile }) => {
         <Projects isMobile={isMobile} />
         <div className={styles.verticalLine}></div>
         <Expertise />
-        <Testimonials />
+        <TestimonialsCarousel testimonials={TestimonialsList} />
         <Definition />
         <Contact />
       </main>
