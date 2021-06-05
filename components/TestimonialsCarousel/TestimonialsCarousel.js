5ee4e680b0d2332c09c41fc96ab35b3960814c25
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-06-04 21:27:29 -0500

    Added testimonials fade animation

--- components/TestimonialsCarousel/TestimonialsCarousel.js
+++ components/TestimonialsCarousel/TestimonialsCarousel.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import styles from "./TestimonialsCarousel.module.css";
 
-const delay = 15000;
+const DELAY = 15000;
 
 const TestimonialCarousel = ({ testimonials, color = "tertiary" }) => {
   const [index, setIndex] = useState(0);
@@ -20,7 +20,7 @@ const TestimonialCarousel = ({ testimonials, color = "tertiary" }) => {
         setIndex((prevIndex) =>
           prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1
         ),
-      delay
+      DELAY
     );
 
     return () => {
@@ -29,31 +29,37 @@ const TestimonialCarousel = ({ testimonials, color = "tertiary" }) => {
   }, [index]);
 
   return (
-    <section className={`${styles.container} ${styles[`${color}Container`]} padding-x`}>
-      <h3 className={styles.title}>Impacting teams, products and thousands of people</h3>
-      <div
-        className={styles.slideshowSlider}
-        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-      >
-        {testimonials.map((testimonial, index) => (
-          <article key={index} className={styles.slide}>            
-            <p className={styles.text}>{testimonial.feedback}</p>
-            <div className={styles.customers}>
-              <img
-                className={styles.customerPicture}
-                src={testimonial.customer.picture}
-                alt={testimonial.customer.name}
-              />
-              <div className={styles.customerInfo}>
-                <span className={styles.customerName}>
-                  {testimonial.customer.name}
-                </span>
-                <span className={styles.customerPosition}>
-                  {testimonial.customer.position}
-                </span>
-              </div>
+    <section
+      className={`${styles.container} ${styles[`${color}Container`]} padding-x`}
+    >
+      <h3 className={styles.title}>
+        Impacting teams, products and thousands of people
+      </h3>
+      <div className={styles.slideshowSlider}>
+        {testimonials.map((testimonial, idx) => (
+          <article
+          key={idx}
+          className={`${styles.slide} ${
+            index === idx ? styles.active : ""
+          }`}
+        >
+          <p className={styles.text}>{testimonial.feedback}</p>
+          <div className={styles.customers}>
+            <img
+              className={styles.customerPicture}
+              src={testimonial.customer.picture}
+              alt={testimonial.customer.name}
+            />
+            <div className={styles.customerInfo}>
+              <span className={styles.customerName}>
+                {testimonial.customer.name}
+              </span>
+              <span className={styles.customerPosition}>
+                {testimonial.customer.position}
+              </span>
             </div>
-          </article>
+          </div>
+        </article>
         ))}
       </div>
 
@@ -66,6 +72,7 @@ const TestimonialCarousel = ({ testimonials, color = "tertiary" }) => {
             }`}
             onClick={() => {
               setIndex(idx);
+              console.log('holi');
             }}
           ></div>
         ))}
