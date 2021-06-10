c542c7e3466ba0a5a81db4807c1d8d06071eac4a
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2021-06-10 01:26:28 -0500

    Added swipe effect to testimonials in contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import ContactForm from "../components/ContactForm/ContactForm";
 import TestimonialsCarousel from "../components/TestimonialsCarousel/TestimonialsCarousel";
-import TestimonialsList from "../content/testimonials.json"
+import TestimonialsList from "../content/testimonials.json";
+import { isMobile } from "../utils";
 import styles from "../styles/Contact.module.css";
 
- const  Contact = () => {
+const Contact = ({ isMobile }) => {
   return (
     <div>
       <Head>
@@ -21,16 +22,28 @@ import styles from "../styles/Contact.module.css";
             <img src="/images/contact-hero.png" alt="Contact hero image" />
           </figure>
           <div>
-            <h1 className={styles.heroTitle}>Let's <span>Talk</span>.</h1>
-            <p className={styles.heroDescription}>Let us know who you are and what you’re up to.</p>
+            <h1 className={styles.heroTitle}>
+              Let's <span>Talk</span>.
+            </h1>
+            <p className={styles.heroDescription}>
+              Let us know who you are and what you’re up to.
+            </p>
             {/* <h3 className={styles.heroSubtitle}>check out my methodology ></h3> */}
           </div>
         </section>
         <ContactForm />
-        <TestimonialsCarousel testimonials={TestimonialsList} color="info" />
+        <TestimonialsCarousel testimonials={TestimonialsList} color="info" isMobile={isMobile} />
       </main>
     </div>
   );
-}
+};
+
+export const getServerSideProps = async ({ req }) => {
+  return {
+    props: {
+      isMobile: isMobile(req),
+    },
+  };
+};
 
 export default Contact;
