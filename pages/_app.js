7ddce51ec57d13ab83dc3e9fc877cc220b23058b
Caro Guerrero <caroguerrerosilvera@gmail.com>
2021-08-03 22:05:13 -0500

    feat: deactive layout only for project page

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
+import { useRouter } from "next/router";
 import Layout from "../layouts/default";
 import { MenuMobileProvider } from "../contexts/mobileMenuContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const { pathname } = router;
+  console.log(pathname);
   return (
     <MenuMobileProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {pathname.includes('/project') && <Component {...pageProps} />}
+      {
+      !pathname.includes('/project') &&
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      }
     </MenuMobileProvider>
   );
 }
