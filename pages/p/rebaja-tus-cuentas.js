ffddac3a8fa7efdb57a5e98bbbf78b8d1b2fe8da
Caro Guerrero <caroguerrerosilvera@gmail.com>
2021-08-29 17:46:20 -0500

    fix: add gradient background to some section (rtc page)

--- pages/p/rebaja-tus-cuentas.js
+++ pages/p/rebaja-tus-cuentas.js
@@ -35,9 +35,11 @@ const RebajaTusCuentasPage = ({ isMobile }) => {
           buttonStyle="info"
         />
         <main className={styles.main}>
-          <figure><img src="/images/projects/rtc-mobile.png" className={styles.videoImage} /></figure>
-          <h1 className={styles.title}>A financial platform  for mortgage loans</h1>
-          <div className={styles.verticalLine}></div>
+          <section className={styles.hero}>
+            <figure><img src="/images/projects/rtc-mobile.png" className={styles.videoImage} /></figure>
+            <h1 className={styles.title}>A financial platform  for mortgage loans</h1>
+            <div className={styles.verticalLine}></div>
+          </section>
           <section className={styles.description}>
             <span className={styles.bicolorTitle}>
               Why <span className="info">RTC?</span>
@@ -109,95 +111,99 @@ const RebajaTusCuentasPage = ({ isMobile }) => {
               company="RebajaTusCuentas.com"
             />
           </section>
-          <section className={styles.beforeStartingSection}>
-            <h3 className={`${styles.sectionTitle} pb-2 px-1`}>Before starting</h3>
-            <img src="/images/projects/rtc-before.png" alt="Products module" className={styles.videoImage} />
-            <p className={`${styles.normalParagraph} px-1 pb-4`}>We started by understanding the entire product up to that point, both users and services and products of RebajaTusCuentas.</p>
-            <figure>
-              <img src="/images/projects/rtc-analytics.webp" alt="Product metrics" className={styles.videoImage} />
-            </figure>
-            <h3 className={`${styles.sectionTitle} px-1 pt-3`}>Let’s start by analyzing metrics and conversations</h3>
-            <p className={`${styles.normalParagraph} px-1`}>RebajaTusCuentas had started a previous research about two weeks before I joined the team. We analyzed all the information gathred to create the first prototypes and start iterating and talking with users with a tangible base.</p>
-          </section>
-          <section className={styles.funelSection}>
-            <div className={`${styles.bicolorTitle} text-centered pb-2`}>
-              Funel <span className="info">of RTC</span>
-            </div>
-            <div className={styles.features}>
-              <div className={styles.feature}>
-                <img src="/images/projects/trc-funel-01.png" alt="Website" />
-                <div>Website</div>
+          <div className={styles.backgroundGradient}>
+            <section className={styles.beforeStartingSection}>
+              <h3 className={`${styles.sectionTitle} pb-2 px-1`}>Before starting</h3>
+              <img src="/images/projects/rtc-before.png" alt="Products module" className={styles.videoImage} />
+              <p className={`${styles.normalParagraph} px-1 pb-4`}>We started by understanding the entire product up to that point, both users and services and products of RebajaTusCuentas.</p>
+              <figure>
+                <img src="/images/projects/rtc-analytics.webp" alt="Product metrics" className={styles.videoImage} />
+              </figure>
+              <h3 className={`${styles.sectionTitle} px-1 pt-3`}>Let’s start by analyzing metrics and conversations</h3>
+              <p className={`${styles.normalParagraph} px-1`}>RebajaTusCuentas had started a previous research about two weeks before I joined the team. We analyzed all the information gathred to create the first prototypes and start iterating and talking with users with a tangible base.</p>
+            </section>
+            <section className={styles.funelSection}>
+              <div className={`${styles.bicolorTitle} text-centered pb-2`}>
+                Funel <span className="info">of RTC</span>
               </div>
-              <div className={styles.feature}>
-                <img src="/images/projects/trc-funel-02.png" alt="Registration" />
-                <div>Registration</div>
+              <div className={styles.features}>
+                <div className={styles.feature}>
+                  <img src="/images/projects/trc-funel-01.png" alt="Website" />
+                  <div>Website</div>
+                </div>
+                <div className={styles.feature}>
+                  <img src="/images/projects/trc-funel-02.png" alt="Registration" />
+                  <div>Registration</div>
+                </div>
+                <div className={styles.feature}>
+                  <img src="/images/projects/trc-funel-03.png" alt="Mortgage type" />
+                  <div>Mortgage type</div>
+                </div>
+                <div className={styles.feature}>
+                  <img src="/images/projects/trc-funel-04.png" alt="Pre-approved loans" />
+                  <div>Pre-approved loans</div>
+                </div>
+                <div className={styles.feature}>
+                  <img src="/images/projects/trc-funel-05.png" alt="Document submission" />
+                  <div>Document submission</div>
+                </div>
+                <div className={styles.feature}>
+                  <img src="/images/projects/trc-funel-06.png" alt="Final evaluation" />
+                  <div>Final evaluation</div>
+                </div>
+                <div className={styles.feature}>
+                  <img src="/images/projects/trc-funel-07.png" alt="Delivery of credit" />
+                  <div>Delivery of credit</div>
+                </div>
               </div>
-              <div className={styles.feature}>
-                <img src="/images/projects/trc-funel-03.png" alt="Mortgage type" />
-                <div>Mortgage type</div>
-              </div>
-              <div className={styles.feature}>
-                <img src="/images/projects/trc-funel-04.png" alt="Pre-approved loans" />
-                <div>Pre-approved loans</div>
-              </div>
-              <div className={styles.feature}>
-                <img src="/images/projects/trc-funel-05.png" alt="Document submission" />
-                <div>Document submission</div>
-              </div>
-              <div className={styles.feature}>
-                <img src="/images/projects/trc-funel-06.png" alt="Final evaluation" />
-                <div>Final evaluation</div>
-              </div>
-              <div className={styles.feature}>
-                <img src="/images/projects/trc-funel-07.png" alt="Delivery of credit" />
-                <div>Delivery of credit</div>
-              </div>
-            </div>
-          </section>
-          <section className={styles.marketSection}>
-            <h3 className={`${styles.sectionTitle} px-1 pb-2`}>We improve market perception by providing confidence</h3>
-            <ul className={`${styles.marketPoints} px-1 pt-1`}>
-              <li>
-                <img src="/icons/check-input.svg" />
-                <span>The roadmap was created by a team. Taking into account time to experiment with prototypes.</span>
-              </li>
-              <li>
-                <img src="/icons/check-input.svg" />
-                <span>We had up to 3 experiments to achieve the best registration process.</span>
-              </li>
-              <li>
-                <img src="/icons/check-input.svg" />
-                <span>Our type of user was more prone to a phone call than to a videoconference. A very interesting type of user for a fintech.</span>
-              </li>
-            </ul>
-            <figure className="text-centered">
-              <img src="/images/projects/rtc-mobile-capture.webp" className={`${styles.videoImage} ${styles.marketSectionImage}`} />
-            </figure>
-          </section>
-          <section className={styles.resultsSection}>
-            <h3 className={`${styles.sectionTitle} pb-2`}>The result was excellent</h3>
-            <figure>
-              <img src="/images/projects/rtc-gallery.png" alt="Gallery" className={styles.videoImage} />
-            </figure>
-            <ul className={`${styles.marketPoints} px-1 pt-2`}>
-              <li>
-                <img src="/icons/check-input.svg" />
-                <span>We unify all RebajaTusCuentas.com services under a single identity.</span>
-              </li>
-              <li>
-                <img src="/icons/check-input.svg" />
-                <span>Expansion to Peru, Colombia and Mexico.</span>
-              </li>
-              <li>
-                <img src="/icons/check-input.svg" />
-                <span>In Peru we went from being a mortgage broker to delivering mortgages directly.</span>
-              </li>
-              <li>
-                <img src="/icons/check-input.svg" />
-                <span>BBVA Peru acquires part of RTC’s operation.</span>
-              </li>
-            </ul>
-          </section>
+            </section>
+          </div>
+          <div className={styles.backgroundGradient}>
+            <section className={styles.marketSection}>
+              <h3 className={`${styles.sectionTitle} px-1 pb-2`}>We improve market perception by providing confidence</h3>
+              <ul className={`${styles.marketPoints} px-1 pt-1`}>
+                <li>
+                  <img src="/icons/check-input.svg" />
+                  <span>The roadmap was created by a team. Taking into account time to experiment with prototypes.</span>
+                </li>
+                <li>
+                  <img src="/icons/check-input.svg" />
+                  <span>We had up to 3 experiments to achieve the best registration process.</span>
+                </li>
+                <li>
+                  <img src="/icons/check-input.svg" />
+                  <span>Our type of user was more prone to a phone call than to a videoconference. A very interesting type of user for a fintech.</span>
+                </li>
+              </ul>
+              <figure className="text-centered">
+                <img src="/images/projects/rtc-mobile-capture.webp" className={`${styles.videoImage} ${styles.marketSectionImage}`} />
+              </figure>
+            </section>
+            <section className={styles.resultsSection}>
+              <h3 className={`${styles.sectionTitle} pb-2`}>The result was excellent</h3>
+              <figure>
+                <img src="/images/projects/rtc-gallery.png" alt="Gallery" className={styles.videoImage} />
+              </figure>
+              <ul className={`${styles.marketPoints} px-1 pt-2`}>
+                <li>
+                  <img src="/icons/check-input.svg" />
+                  <span>We unify all RebajaTusCuentas.com services under a single identity.</span>
+                </li>
+                <li>
+                  <img src="/icons/check-input.svg" />
+                  <span>Expansion to Peru, Colombia and Mexico.</span>
+                </li>
+                <li>
+                  <img src="/icons/check-input.svg" />
+                  <span>In Peru we went from being a mortgage broker to delivering mortgages directly.</span>
+                </li>
+                <li>
+                  <img src="/icons/check-input.svg" />
+                  <span>BBVA Peru acquires part of RTC’s operation.</span>
+                </li>
+              </ul>
+            </section>
+          </div>
           <section className={styles.moreProjectsSection}>
             <h3 className={`${styles.sectionTitle} text-white`}>Discover more project</h3>
             <div className={styles.projectsSection}>
@@ -211,11 +217,11 @@ const RebajaTusCuentasPage = ({ isMobile }) => {
                 />
               </div>
             </div>
-            {/* <Link href="/">
+            <Link href="/">
               <button className="black">
                 View all projects
               </button>
-            </Link> */}
+            </Link>
           </section>
           <TestimonialsCarousel testimonials={TestimonialsList} isMobile={isMobile} />
           <section className={styles.primaryBackground}>
