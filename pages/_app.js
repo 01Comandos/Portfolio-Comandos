6fcdd7def2e47775aef7d214091d2feffe580f8a
Caro Guerrero <caroguerrerosilvera@gmail.com>
2021-11-14 21:16:49 -0500

    update header on countdown page

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
+import { useRouter } from "next/router";
 import Layout from "../layouts/default";
 import { MenuMobileProvider } from "../contexts/mobileMenuContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const { pathname } = router;
   return (
     <MenuMobileProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {pathname.includes("/p/") && <Component {...pageProps} />}
+      {!pathname.includes("/p/") && (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </MenuMobileProvider>
   );
 }
