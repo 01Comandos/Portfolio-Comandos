ce85c5dca85ad20f0bec1e48fd7bbcc691dc6664
Caro Guerrero <caroguerrerosilvera@gmail.com>
2021-11-14 21:11:07 -0500

    update hero

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,13 @@
-import { useRouter } from "next/router";
 import Layout from "../layouts/default";
 import { MenuMobileProvider } from "../contexts/mobileMenuContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const { pathname } = router;
-
   return (
     <MenuMobileProvider>
-      {pathname.includes('/p/') && <Component {...pageProps} />}
-      {
-      !pathname.includes('/p/') &&
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      }
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </MenuMobileProvider>
   );
 }
