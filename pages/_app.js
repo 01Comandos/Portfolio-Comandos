251e133a545a85129c68c538ffe47306594c2016
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2022-01-27 18:40:00 -0300

    hotjar config added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import { useRouter } from 'next/router';
-import Layout from '../layouts/default';
-import { MenuMobileProvider } from '../contexts/mobileMenuContext';
-import '../styles/globals.css';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { trackEvent } from '@/analytics/events';
+import { useRouter } from "next/router";
+import Layout from "../layouts/default";
+import { MenuMobileProvider } from "../contexts/mobileMenuContext";
+import "../styles/globals.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import { trackEvent } from "@/analytics/events";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -13,7 +14,7 @@ function MyApp({ Component, pageProps }) {
   const [history, setHistory] = useState([]);
 
   const handleRouteChange = (url, context) => {
-    if (url.includes('/contact')) {
+    if (url.includes("/contact")) {
       const [firstUrl = null] = history;
 
       if (firstUrl) {
@@ -27,12 +28,12 @@ function MyApp({ Component, pageProps }) {
   };
 
   useEffect(() => {
-    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on("routeChangeStart", handleRouteChange);
 
     // If the component is unmounted, unsubscribe
     // from the event with the `off` method:
     return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off("routeChangeStart", handleRouteChange);
     };
   }, [router]);
 
@@ -42,8 +43,24 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <MenuMobileProvider>
-      {pathname.includes('/p/') && <Component {...pageProps} />}
-      {!pathname.includes('/p/') && (
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:2803762,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  `,
+          }}
+        />
+      </Head>
+      {pathname.includes("/p/") && <Component {...pageProps} />}
+      {!pathname.includes("/p/") && (
         <Layout>
           <Component {...pageProps} />
         </Layout>
